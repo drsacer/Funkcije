e51f5089226ea7be411cb6ea368c001e2f229fc2
drsacer <drsacer@gmail.com>
2022-02-22 14:31:09 +0100

    Isto ime funkcije, tip i broj argumenata (ali je u jednoj argument tipa double, a u drugoj float).

--- src/Funkcije/Kruznica.java
+++ src/Funkcije/Kruznica.java
@@ -16,7 +16,8 @@ public class Kruznica {
 
         switch (unos){
             case "O":
-                izracunajOpseg(r);
+                double rr = s.nextDouble();
+                izracunajOpseg(rr);   //  šaljemo double rr pa se pokreće funkcija static void izracunajOpseg(double r)
                 break;
             case "P":
                 System.out.println(izracunajPovrsinu(r));
@@ -24,14 +25,6 @@ public class Kruznica {
             default:
                 System.out.println("Krivi odabir");
         }
-
-     /*   System.out.println("POV:" + izracunajPovrsinu(r));
-        float pov = izracunajPovrsinu(r);
-        System.out.println("POV:" + pov);
-
-        System.out.println("POV1:" + izracunajPovrsinu(r,Math.PI));
-
-        izracunajOpseg(r);*/
     }
 
     static float izracunajPovrsinu(float r){
@@ -43,13 +36,15 @@ public class Kruznica {
         return (float)(r*r*Math.PI);
     }
 
-    /* static double izracunajPovrsinu(float r, double pi){
-        return (float)(r*r*Math.PI);
-    }
-    u potpis metode ide ime metode i argumenti*/
+    // u potpis metode ide ime metode i argumenti
 
     static void izracunajOpseg(float r){
         float opseg = (float) (2*r*Math.PI);
-        System.out.println("OPSEG:" + opseg);
+        System.out.println("FLOAT - OPSEG:" + opseg);
+    }
+
+    static void izracunajOpseg(double r){
+        float opseg = (float) (2*r*Math.PI);
+        System.out.println("DOUBLE - OPSEG:" + opseg);
     }
 }
